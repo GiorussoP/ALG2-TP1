b8c97003452a007fa640a12c389dc3e37f5af8a2
8JIPT <142136375+8JIPT@users.noreply.github.com>
2025-05-31 00:10:20 -0300

    correcao e atualizacao da 2dtree; atualizacao dos testes desta; add functs p/ auxiliar o uso dos dados c/ a 2dtree

--- _2d_tree.py
+++ _2d_tree.py
@@ -18,65 +18,58 @@ class _2d_tree:
 
     def __build_tree__(self, p_ord_lat, p_ord_lon, depth):
         size = len(p_ord_lat)
-        if size == 0:
+        if size == 0 or len(p_ord_lon) == 0:
             return None
         if size == 1:
             return p_ord_lat[0]
+
+        lat_left = []
+        lat_right = []
+        lon_left = []
+        lon_right = []
+
+        if depth % 2 != 0:
+            #corte vertical: latitude
+            median = size // 2
+            vertex = p_ord_lat[median]
+
+            lat_left = p_ord_lat[:median]
+            lat_right = p_ord_lat[median+1:]
+
+            #pegar os IDs das listas esquerda/direita
+            ids_left = set(p.ID for p in lat_left)
+            ids_right = set(p.ID for p in lat_right)
+
+            #manter consistência com p_ord_lon
+            lon_left = [p for p in p_ord_lon if p.ID in ids_left]
+            lon_right = [p for p in p_ord_lon if p.ID in ids_right]
         else:
-            lat_left = []
-            lat_right = []
-            lon_left = []
-            lon_right = []
-            if depth % 2 != 0:
-                #vertical cut: latitude
-                median = size // 2
-                vertex = p_ord_lat[median]
+            #corte horizontal: longitude
+            median = size // 2
+            vertex = p_ord_lon[median]
 
-                lat_left = p_ord_lat[:median]
-                lat_right = p_ord_lat[median+1:]
+            lon_left = p_ord_lon[:median]
+            lon_right = p_ord_lon[median+1:]
 
-                #splitting points based on the lat_median
-                for point in p_ord_lon:
-                    if point.lat < vertex.lat:
-                        lon_left.append(point)
-                    elif point.lat > vertex.lat:
-                        lon_right.append(point)
-                    elif point != vertex:
-                        if len(lon_left) <= len(lon_right):
-                            lon_left.append(point)
-                        else:
-                            lon_right.append(point)
-            else:
-                #horizontal cut: longitude
-                median = size // 2
-                vertex = p_ord_lon[median]
+            #pegar os IDs das listas esquerda/direita
+            ids_left = set(p.ID for p in lon_left)
+            ids_right = set(p.ID for p in lon_right)
 
-                lon_left = p_ord_lon[:median]
-                lon_right = p_ord_lon[median+1:]
+            #manter consistência com p_ord_lat
+            lat_left = [p for p in p_ord_lat if p.ID in ids_left]
+            lat_right = [p for p in p_ord_lat if p.ID in ids_right]
 
-                #splitting points based on the lon_median
-                for point in p_ord_lat:
-                    if point.lon < vertex.lon:
-                        lat_left.append(point)
-                    elif point.lon > vertex.lon:
-                        lat_right.append(point)
-                    elif point != vertex:
-                        if len(lat_left) <= len(lat_right):
-                            lat_left.append(point)
-                        else:
-                            lat_right.append(point)
-                
-        vertex.left = (self.__build_tree__(lat_left, lon_left, depth + 1))
-        vertex.right = (self.__build_tree__(lat_right, lon_right, depth + 1))
+        vertex.left = self.__build_tree__(lat_left, lon_left, depth + 1)
+        vertex.right = self.__build_tree__(lat_right, lon_right, depth + 1)
         return vertex
-    
+
     def build(self, points):
-        p_ord_lat = sorted(points, key=lambda points: points.lat)
-        p_ord_lon = sorted(points, key=lambda points: points.lon)
+        p_ord_lat = sorted(points, key=lambda p: p.lat)
+        p_ord_lon = sorted(points, key=lambda p: p.lon)
         self.root = self.__build_tree__(p_ord_lat, p_ord_lon, 0)
 
     def range_search(self, R):
-        #range being (lat_low, lat_upp, lon_low, lon_upp)
+        #range -> (lat_low, lat_upp, lon_low, lon_upp)
         results = []
         self.__search__(self.root, R, 0, results)
         return results
@@ -85,33 +78,31 @@ class _2d_tree:
         if vertex is None:
             return
 
-        if vertex.left is None and vertex.right is None:
-            #vertex is a leaf
-            if vertex.in_range(R):
-                #vertex in range
-                results.append(vertex)
-            return
+        if vertex.in_range(R):
+            results.append(vertex)
 
         if depth % 2 == 0:
-            #splitting on latitude
-            if vertex.in_range(R):
-                results.append(vertex)
-            
-            #checking if lc or rc could intersect R
-            if vertex.left and R[0] <= vertex.lat:
-                self.__search__(vertex.left, R, depth + 1, results)
-            if vertex.right and R[1] >= vertex.lat:
-                self.__search__(vertex.right, R, depth + 1, results)
-        else:
-            #splitting on longitude
-            if vertex.in_range(R):
-                results.append(vertex)
-
-            #checking if lc or rc could intersect R
+            #divisão em longitude (corte horizontal)
             if vertex.left and R[2] <= vertex.lon:
                 self.__search__(vertex.left, R, depth + 1, results)
             if vertex.right and R[3] >= vertex.lon:
                 self.__search__(vertex.right, R, depth + 1, results)
+        else:
+            #divisão em latitude (corte vertical)
+            if vertex.left and R[0] <= vertex.lat:
+                self.__search__(vertex.left, R, depth + 1, results)
+            if vertex.right and R[1] >= vertex.lat:
+                self.__search__(vertex.right, R, depth + 1, results)
+
+    
+    def __len__(self):
+        return self.__count_nodes__(self.root)
+
+    def __count_nodes__(self, node):
+        if node is None:
+            return 0
+        return 1 + self.__count_nodes__(node.left) + self.__count_nodes__(node.right)
+
 
 
 
--- testes/test_2d_tree.py
+++ testes/test_2d_tree.py
@@ -1,18 +1,68 @@
 from _2d_tree import node, _2d_tree
+from tree_csv_aux_functions import build_aux_structures, format_results
 
 if __name__ == "__main__":
+
+    print("Teste pequeno e artificial")
+
     points = [
-        node(1, 10.0, 20.0),
-        node(2, 15.0, 25.0),
-        node(3, 5.0, 30.0),
-        node(4, 7.0, 10.0),
-        node(5, 12.0, 15.0),
+    node("A", -23.550520, -46.633308),
+    node("B", -23.550521, -46.633307),
+    node("C", -23.550522, -46.633306),
+    node("D", -23.550523, -46.633305),
+    node("E", -23.550524, -46.633304)
     ]
+
+    tree_artf = _2d_tree()
+    tree_artf.build(points)
+    search_range = (-23.550523, -23.550520, -46.633308, -46.633306)
+
+    results = tree_artf.range_search(search_range)
+    for node in results:
+        print(node)
+
+    print("\n\tTeste com dados completos\n")
+
+    path = "./bares_restaurantes.csv"
+
+    data_vector = []
+    points = []
+    points, data_vector = build_aux_structures(path)
 
     tree = _2d_tree()
     tree.build(points)
 
-    #search range: lat from 6 to 16, lon from 10 to 26
-    results = tree.range_search((6, 16, 14, 26))
-    for r in results:
-        print(r)
+    print(tree.__len__())
+
+    print("\n\ttestando para range sem elementos")
+    result = tree.range_search((-19.911468,-19.909732, -43.954193, -43.951125))
+    print(len(result))
+    print(result)
+    print(format_results(result, data_vector))
+
+    print("\ttestando para um unico elemento ")
+    result = tree.range_search((-19.959328, -19.959328, -44.001602, -44.001602))
+    print(len(result))
+    print(result)
+    print(format_results(result, data_vector))
+
+    print("\ttestando para um pequeno range")
+    result = tree.range_search((-19.909574, -19.907233, -43.965182, -43.961234))
+    print(len(result))
+    print(result)
+    print(format_results(result[:3], data_vector))
+
+    print("\ttestando para range moderado")
+    result = tree.range_search((-19.926894, -19.916077,-43.947619, -43.933285))
+    print(len(result))
+    print(result[:3])
+    print(format_results(result[:3], data_vector))
+
+    print("\ttestando para o maior range (todos os pontos)")
+    result = tree.range_search((-20.069097,-19.765567 ,-44.211557, -43.702066))
+    print(len(result))
+    print(result[:3])
+    print(format_results(result[:3], data_vector))
+
+
+        
